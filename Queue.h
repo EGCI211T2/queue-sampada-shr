b2b1d477611b665d972c4192c75eb112e1f6cc62
Sam <sampadashrestha315@gmail.com>
2026-03-11 04:14:20 +0000

    v0

--- Queue.h
+++ Queue.h
@@ -15,21 +15,29 @@ public:
 
 void Queue::enqueue(int x){
   NodePtr new_node= new NODE(x);
-if(new_node){ 
+if(new_node!=NULL){ 
+    if(size>0) {tailPtr->set_next(new_node); }
+    else {headPtr=new_node;}
+    tailPtr=new_node; size++;
     /* Add head and tail for me please */
-   1. connect & Change tail
-  2. (may be) change head  when the queue is empty
-  3. increase size */
-	
+  // 1. connect & Change tail
+ // 2. (may be) change head  when the queue is empty
+ // 3. increase size */
  }
+ else{ cout<<"no memory.\n";}
 }
 
 int Queue::dequeue(){
   if(size>0){
-    
+    if (headPtr!=NULL){
+            NodePtr t=headPtr;
+            int value= t->get_value();
+            headPtr= headPtr->get_next();
+            size--;
+            delete t;
+            return value;
+    }
      /* Add head and tail for me please */
-          
-    
   }
   cout<<"Empty queue";
   return -1;
@@ -37,13 +45,15 @@ int Queue::dequeue(){
 
 
 Queue::Queue(){
-    //initialize Queue
+    size=0;
+    headPtr=NULL;
+    tailPtr=NULL;
     
 }
 Queue::~Queue(){
-      
+      cout<<"Dequeue all"<<endl;
     //delete all remaning Queue (i.e. DQ all) 
-    
+    for(int i=0;i<size;i++) dequeue();
 }
 
 
